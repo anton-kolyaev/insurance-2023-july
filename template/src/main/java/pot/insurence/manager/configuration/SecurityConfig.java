5bee0455213a738f302e75c8cf635478717102a8
NorthDragonDuck <113013199+NorthDragonDuck@users.noreply.github.com>
2023-06-20 15:12:58 +0300

    [POT-6] Configure liquibase  and add User table (#3)
    * Added a user table for future work with Liquibase
    
    * Initial Liquibase setup
    
    * Changed username and password
    
    * Liquibase configuration for handling multiple .sql scripts. Also changes the .sql script to create a user table inside the java_internship schema.
    
    * Converted .properties file to .yaml. Removed unwanted code in the database specification and returned the version of h2 dependency in the .gradle file to setted by default.
    
    ---------
    
    Co-authored-by: pkondratas <paulius.kondratas@gmail.com>

--- template/src/main/java/pot/insurence/manager/configuration/SecurityConfig.java
+++ template/src/main/java/pot/insurence/manager/configuration/SecurityConfig.java
@@ -24,7 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests((authorizeRequests) ->
+            .authorizeHttpRequests((authorizeRequests) ->
                 authorizeRequests
                     .requestMatchers("/admin/**").hasRole("ADMIN")
                     .requestMatchers("/v1/echo").hasAnyRole("USER", "ADMIN")
